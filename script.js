c8536918579d7d2dd0760546786de048ecf54961
Ewa Maj <awemaj@gmail.com>
2018-02-24 13:42:45 +0100

    save

--- script.js
+++ script.js
@@ -7,4 +7,26 @@ hello('Mateusz', 1)
 hello('Ala', 2)
 hello('Ela', 3)
 
+var secondsCounter = 1
 
+var intervalID = setInterval (
+    function (){
+        console.log(secondsCounter++)
+    },
+    1000
+)
+
+var globalVariable = 'GLOBAL'
+
+function add(a, b){
+    var someVariable = 123
+    globalVariable = 321
+    console.log(globalVariable)
+    return a + b
+}
+
+var result = add(3,5)
+
+console.log(result)
+
+console.log(someVariable)
