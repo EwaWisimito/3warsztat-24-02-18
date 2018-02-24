17dae62908f288e902bbfcc0cd0435fd54046f7e
Ewa Maj <awemaj@gmail.com>
2018-02-24 15:02:41 +0100

    klik

--- script.js
+++ script.js
@@ -1,34 +1,10 @@
+var body = document.querySelector('body')
 
+console.log(body)
 
-(function(){
-var secondsCounter = 1
-
-var intervalID = setInterval (
-    function (){
-        console.log(secondsCounter++)
-    },
-    1000
-)
-
-setTimeout(
+body.addEventListener(
+    'click',
     function(){
-        clearInterval(intervalID)
-    },
-    5000
+        console.log('Klik!')
+    }
 )
-})()
-
-var globalVariable = 'GLOBAL'
-
-function add(a, b){
-    var someVariable = 123
-    globalVariable = 321
-    console.log(globalVariable)
-    return a + b
-}
-
-var result = add(3,5)
-
-console.log(result)
-
-console.log(someVariable)
