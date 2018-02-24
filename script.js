b157e5d96084f79b9a0c4107adfe31b735c8a58c
Ewa Maj <awemaj@gmail.com>
2018-02-24 12:50:46 +0100

    numer 3

--- script.js
+++ script.js
@@ -23,3 +23,32 @@ console.log(nullVariable)
 //undefined
 var undefinedVariable = undefined
 console.log(undefinedVariable)
+
+//objects
+
+//objects with data
+var simplestObj = {
+    name: "Ewa",
+    lastname: "Maj"
+}
+console.log(simplestObj)
+
+//nested object with data
+
+var nestedObj = {
+    name: "Ewa",
+    car: {
+        brand: "Peugeot",
+        model: 407
+    }
+}
+console.log(nestedObj)
+console.log(nestedObj.car.model)
+
+//arrays
+var simpleArray = [1,2,
+    {name: 'Ala', array: [1,4,6]},
+    3,4,5]
+console.log(simpleArray)
+console.log(simpleArray[4])
+console.log(simpleArray[1].name)
