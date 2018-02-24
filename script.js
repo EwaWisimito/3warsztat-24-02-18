be2dca318fed2aaa7f3948ba9ffd04bedf1562f8
Ewa Maj <awemaj@gmail.com>
2018-02-24 15:36:39 +0100

    mole lata

--- script.js
+++ script.js
@@ -2,12 +2,20 @@
 
 
     function makeMole(){
+
+        var molePosX = Math.round( 
+            Math.random()*(window.innerWidth - window.innerHeight /10)
+         )
+        var molePosY = Math.round( 
+            Math.random()*(window.innerHeight - window.innerHeight /10)
+         )
+
         var mole = document.createElement('div')
 
         mole.classList.add('mole')
 
-        mole.style.left = '100px'
-        mole.style.top = '100px'
+        mole.style.left = molePosX + 'px'
+        mole.style.top = molePosY + 'px'
 
         document.querySelector('body').appendChild(mole)
     }
